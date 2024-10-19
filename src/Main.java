9ec1a3a28181b28621528219417947a93399e7c5
O.Gogol <o.gogol91@gmail.com>
2024-10-19 13:13:21 +0100

    Initial commtit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 158577;
+        int milePrice = 20;
+        int bonus = ticketPrice / milePrice;
+        System.out.println(bonus);
+    }
+}
